8d08dbeda79dd3289dbb3f64556d153073ceb7bc
CongPhan <anhcong97ht@gmail.com>
2024-05-27 22:35:09 +0700

    Create map.go

---
+++ Go Lang/LAB/map.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	name := make(map[string]int)
+	name["congpv"] = 120897
+	name["Hau"] = 100698
+	name["Minh"] = 250823
+	fmt.Println("ten va so :", name)
+}
