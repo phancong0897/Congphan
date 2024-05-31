db80e14a08f50530c49d26e4d0bbee2e716afac2
CongPhan <46736124+phancong0897@users.noreply.github.com>
2024-05-31 11:00:09 +0700

    Update string.go

--- Go Lang/LAB/string.go
+++ Go Lang/LAB/string.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"unicode/utf8"
 )
 
 func printBytes(s string) {
@@ -23,6 +24,15 @@ func printCharsAndBytes(s string) {
 	}
 }
 
+func length(s string) {
+	fmt.Printf("length of %s is %d \n", s, utf8.RuneCountInString(s))
+}
+
+func mutate(s []rune) string {
+	s[0] = 'p' // bất kỳ ký tự unicode hợp lệ trong ngoặc nháy đơn đều là một rune
+	return string(s)
+}
+
 func main() {
 	name := "Phan Văn Công"
 	fmt.Println(name)
@@ -41,4 +51,9 @@ func main() {
 	name3slice := []rune{0x43, 0x6f, 0x6e, 0x67}
 	name3 := string(name3slice)
 	fmt.Println(name3)
+	length(name)
+	length(name1)
+	length(name2)
+	fmt.Println(name)
+	fmt.Print(mutate([]rune(name)))
 }
